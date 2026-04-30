82b4ba815ae3f7787bcf8dede94c28d4c6fe3c05
ValentinMayborda <valentin.mayboroda@gmail.com>
2026-04-30 15:54:24 +0300

    Add tasks for 2 lessons

--- Lesson_3/task1.py
+++ Lesson_3/task1.py
@@ -2,3 +2,5 @@ name = 'Valentyn'
 day = 'Wednesday'
 
 print(f'Good day {name}! {day} is a perfect day to learn some python.')
+print('Good day {}! {} is a perfect day to learn some python.'.format(name, day))
+
