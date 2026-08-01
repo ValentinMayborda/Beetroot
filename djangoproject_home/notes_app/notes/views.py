82041d725109c0092b4aea39d2e53f8ab0fad6ab
ValentinMayborda <valentin.mayboroda@gmail.com>
2026-08-01 21:38:56 +0300

    Оновлення під завдання 2.
    Створити головну html сторінку з нотатками. Створити view із тестовими даними.
    
    Extra:
    Винести css у окремий файл в папку static.

--- djangoproject_home/notes_app/notes/views.py
+++ djangoproject_home/notes_app/notes/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# def home(request):
+#     return HttpResponse("Hello from Notes app")
 
 def home(request):
-    return HttpResponse("Hello from Notes app")
+    context = {
+        'task1': "Переглянути перший урок по Django",
+        'task2': "Зробити домашнє завдання",
+        'task3': "Вивчати шаблони Django",
+        'task4': "Створити моделі",
+        'task5': "Додати функціонування",
+
+    }
+
+    return render(request, 'index.html', context)
 
